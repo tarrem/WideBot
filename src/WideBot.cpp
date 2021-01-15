b0296f77cd2c23c7855734c03634879326e9129a
tarrem <tarrem@pm.me>
2021-01-15 09:35:27 -0800

    Add !wide command

--- src/WideBot.cpp
+++ src/WideBot.cpp
@@ -19,50 +19,55 @@ void WideBot::onMessage(Message message) {
     sendMessage(message.channelID, response);
   }
 
-  if (!message.attachments.empty()) {
-    std::cout << "Recieved attachment: " << std::endl;
-    for (const auto& f : message.attachments) {
-      std::cout << '\t' << f.filename << " at " << f.url << std::endl;
-   
-      std::stringstream out;
-      try { 
+  if (message.startsWith("!wide")) {
+    if (message.attachments.empty()) {
+      sendMessage(message.channelID, "Error: no attachment found");
+    } else {
+      std::cout << "Recieved attachment: " << std::endl;
+      for (const auto& f : message.attachments) {
+        std::cout << '\t' << f.filename << " at " << f.url << std::endl;
+     
+        std::stringstream out;
+        try { 
 
-        curl->setOpt(Options::Url(f.url));
-        curl->setOpt(Options::WriteStream(&out));
-        curl->perform();
-      }
-      catch (cURLpp::RuntimeError& e) {
-        std::cerr << e.what() << std::endl;
-        sendMessage(message.channelID, e.what()); // TODO make this friendly
-      }
-      catch (cURLpp::LogicError& e) {
-        std::cerr << e.what() << std::endl;
-        sendMessage(message.channelID, e.what()); // TODO make this friendly
-      }
+          curl->setOpt(Options::Url(f.url));
+          curl->setOpt(Options::WriteStream(&out));
+          curl->perform();
+        }
+        catch (cURLpp::RuntimeError& e) {
+          std::cerr << e.what() << std::endl;
+          sendMessage(message.channelID, e.what()); // TODO make this friendly
+        }
+        catch (cURLpp::LogicError& e) {
+          std::cerr << e.what() << std::endl;
+          sendMessage(message.channelID, e.what()); // TODO make this friendly
+        }
 
-      out.seekg(0, std::ios::end);
-      long size = out.tellg();
-      out.seekg(0, std::ios::beg);
-      char data[size];
-      out.read(data, size);
+        out.seekg(0, std::ios::end);
+        long size = out.tellg();
+        out.seekg(0, std::ios::beg);
+        char data[size];
+        out.read(data, size);
 
-      Magick::Blob blob(data, size);
+        Magick::Blob blob(data, size);
 
-      std::list<Magick::Image> images;
-      try {
-          Magick::readImages(&images, blob);
+        std::list<Magick::Image> image;
+        try {
+            Magick::readImages(&image, blob);
+        }
+        catch (std::exception& e) {
+          std::cerr << e.what() << std::endl;
+        }
+        Magick::Geometry geoOrig(f.width, f.height);
+        Magick::Geometry geoWide1(f.width / 2, f.height);
+        Magick::Geometry geoWide2(f.width, f.height, f.width / 2, 0);
+        for (auto& i : image) {
+          i.crop(geoWide1);
+          i.resize(geoOrig);
+        }
+        Magick::writeImages(image.begin(), image.end(), "wide"+f.filename);
+        uploadFile(message.channelID, "wide"+f.filename, "");
       }
-      catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-      }
-      Magick::Geometry geoOrig(f.width, f.height);
-      Magick::Geometry geoWide1(f.width / 2, f.height);
-      Magick::Geometry geoWide2(f.width, f.height, f.width / 2, 0);
-      for (auto& image : images) {
-//        image.crop(geoWide1);
-//        image.resize(geoOrig); 
-      }
-        Magick::writeImages(images.begin(), images.end(), "test_"+f.filename);
     }
   }
 }
