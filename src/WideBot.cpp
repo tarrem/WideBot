364b2ff2f1c26196000ceb384f760f28be27cbc2
tarrem <tarrem@pm.me>
2021-01-14 19:39:17 -0800

    Read directly into memory, load into and write to file through Magick++

--- src/WideBot.cpp
+++ src/WideBot.cpp
@@ -1,10 +1,14 @@
 #include <WideBot.hpp>
+
 #include <curlpp/Options.hpp>
 #include <curlpp/Exception.hpp>
 #include <Magick++.h>
-#include <vector>
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <list>
+#include <vector>
 
 using namespace SleepyDiscord;
 using namespace cURLpp;
@@ -20,33 +24,45 @@ void WideBot::onMessage(Message message) {
     for (const auto& f : message.attachments) {
       std::cout << '\t' << f.filename << " at " << f.url << std::endl;
    
+      std::stringstream out;
       try { 
-        std::ofstream out(f.filename, std::ios_base::binary | std::ios_base::out);
 
         curl->setOpt(Options::Url(f.url));
         curl->setOpt(Options::WriteStream(&out));
         curl->perform();
-        out.close();
       }
       catch (cURLpp::RuntimeError& e) {
         std::cerr << e.what() << std::endl;
+        sendMessage(message.channelID, e.what()); // TODO make this friendly
       }
       catch (cURLpp::LogicError& e) {
         std::cerr << e.what() << std::endl;
+        sendMessage(message.channelID, e.what()); // TODO make this friendly
       }
 
-      std::vector<Magick::Image> images;
+      out.seekg(0, std::ios::end);
+      long size = out.tellg();
+      out.seekg(0, std::ios::beg);
+      char data[size];
+      out.read(data, size);
+
+      Magick::Blob blob(data, size);
+
+      std::list<Magick::Image> images;
       try {
-        Magick::readImages(&images, f.filename);
+          Magick::readImages(&images, blob);
       }
       catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
       }
+      Magick::Geometry geoOrig(f.width, f.height);
+      Magick::Geometry geoWide1(f.width / 2, f.height);
+      Magick::Geometry geoWide2(f.width, f.height, f.width / 2, 0);
       for (auto& image : images) {
-        //TODO resize each frame
-        
+//        image.crop(geoWide1);
+//        image.resize(geoOrig); 
       }
-      Magick::writeImages(images.begin(), images.end(), "test.gif");
+        Magick::writeImages(images.begin(), images.end(), "test_"+f.filename);
     }
   }
 }
